0f73c7025e0188d3fdb0103ff751f9d3119ef09d
Randy Kinne <r27kinne@gmail.com>
2020-11-19 19:48:47 -0500

    Remove spring instantiation to only load MVC layers

--- src/test/java/dev/randykinne/restservice/DeliveryControllerTests.java
+++ src/test/java/dev/randykinne/restservice/DeliveryControllerTests.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import dev.randykinne.restservice.controller.DeliveryController;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@WebMvcTest
+@WebMvcTest(DeliveryController.class)
 @AutoConfigureMockMvc
 public class DeliveryControllerTests {
 
--- src/test/java/dev/randykinne/restservice/IndexControllerTests.java
+++ src/test/java/dev/randykinne/restservice/IndexControllerTests.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import dev.randykinne.restservice.controller.IndexController;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@WebMvcTest
+@WebMvcTest(IndexController.class)
 @AutoConfigureMockMvc
 public class IndexControllerTests {
 
